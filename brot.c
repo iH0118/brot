f376931d6c13ed9a212b41d6537ec51724d33389
iH0118 <iH0118@users.noreply.github.com>
2023-11-06 17:04:05 +0100

    asda

--- brot.c
+++ brot.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 /*image borders*/
 #define X_MIN -2.125
@@ -9,7 +10,14 @@
 typedef unsigned int uint;
 typedef unsigned char uchar;
 typedef _Bool bool;
-typedef struct rgb24 rgb24;
+typedef union val value_t;
+typedef struct rgb24 rgb24_t;
+typedef struct thread_args thread_args_t;
+
+union val {
+    uint iter_count;
+    float hue;
+};
 
 struct rgb24 {
     uchar r;
@@ -17,8 +25,17 @@ struct rgb24 {
     uchar b;
 };
 
-static rgb24 hue_rgb(float hue) {
-    rgb24 tmp = {
+struct thread_args {
+    value_t** values;
+    uint k;
+    uint scale;
+    uint size_x;
+    uint output_mode;
+    uint max_iter;
+};
+
+static rgb24_t hue_rgb(float hue) {
+    rgb24_t tmp = {
         (uchar) ((hue >= 0.5 ? hue * 2 - 1 : 0) * 255),
         (uchar) (hue * 255),
         (uchar) ((hue <= 0.5 ? hue * 2 : 1) * 255)
@@ -26,13 +43,33 @@ static rgb24 hue_rgb(float hue) {
     return tmp;
 }
 
-static rgb24 palettize(rgb24 color, uint p) {
-    rgb24 tmp = {
+static rgb24_t palettize(rgb24_t color, uint p) {
+    rgb24_t tmp = {
         !(p     % 3) ? color.r : p     % 3 == 1 ? color.g : color.b,
         !(p / 3 % 3) ? color.r : p / 3 % 3 == 1 ? color.g : color.b,
         !(p / 9 % 3) ? color.r : p / 9 % 3 == 1 ? color.g : color.b
     };
     return tmp;
+}
+
+static void* etime_thread(void* argp) {
+    thread_args_t args = *(thread_args_t*)argp;
+    for (uint i = 0; i < args.size_x; ++i) {
+            double x0 = i * (0.125 / args.scale) + X_MIN;
+            double y0 = args.k * ((args.output_mode ? 0.125 : 0.25) / args.scale) - Y_MINMAX;
+            double x = 0, y = 0, x2 = 0, y2 = 0;
+
+            while (x2 + y2 <= 4 && args.values[i][args.k].iter_count < args.max_iter) {
+                y = (x + x) * y + y0;
+                x = x2 - y2 + x0;
+                x2 = x * x;
+                y2 = y * y;
+                ++args.values[i][args.k].iter_count;
+            }
+
+            if (++args.values[i][args.k].iter_count >= args.max_iter) args.values[i][args.k].iter_count = 0;
+        }
+    return NULL;
 }
 
 int main(int argc, char** argv) {
@@ -52,39 +89,39 @@ int main(int argc, char** argv) {
     uint size_yh = 5 * (output_mode + 1) * scale + 1;
 
     /*allocate iteration count / hue union array*/
-    union v {uint iter_count; float hue;}** vals = calloc(size_x, sizeof(union v*));
-    if (vals == NULL) return 1;
+    value_t** values = calloc(size_x, sizeof(value_t*));
+    if (values == NULL) return 1;
 
     for (uint i = 0; i < size_x; ++i) {
-	    vals[i] = calloc(size_y, sizeof(union v));
-        if (vals[i] == NULL) return 1;
+	    values[i] = calloc(size_y, sizeof(value_t));
+        if (values[i] == NULL) return 1;
     }
 
     fprintf(stderr, "%ix%i, %i iterations\n", size_x, size_y, max_iter);
 
     /*calculate escape time*/
-    for (uint k = 0; k < size_yh; ++k) {
+    /*for (uint k = 0; k < size_yh; ++k) {
         for (uint i = 0; i < size_x; ++i) {
             double x0 = i * (0.125 / scale) + X_MIN;
             double y0 = k * ((output_mode ? 0.125 : 0.25) / scale) - Y_MINMAX;
             double x = 0, y = 0, x2 = 0, y2 = 0;
 
-            while (x2 + y2 <= 4 && vals[i][k].iter_count < max_iter) {
+            while (x2 + y2 <= 4 && values[i][k].iter_count < max_iter) {
                 y = (x + x) * y + y0;
                 x = x2 - y2 + x0;
                 x2 = x * x;
                 y2 = y * y;
-                ++vals[i][k].iter_count;
+                ++values[i][k].iter_count;
             }
 
-            if (++vals[i][k].iter_count >= max_iter) vals[i][k].iter_count = 0;
+            if (++values[i][k].iter_count >= max_iter) values[i][k].iter_count = 0;
         }
-    }
+    }*/
 
     /*symmetry*/
     for (uint k = 1; k < size_yh; ++k) {
         for (uint i = 0; i < size_x; ++i) {
-            vals[i][size_yh - 1 + k].iter_count = vals[i][size_yh - 1 - k].iter_count;
+            values[i][size_yh - 1 + k].iter_count = values[i][size_yh - 1 - k].iter_count;
         }
     }
 
@@ -94,7 +131,7 @@ int main(int argc, char** argv) {
 
     for (uint k = 0; k < size_y; ++k) {
         for (uint i = 0; i < size_x; ++i) {
-            ++px_per_iter[vals[i][k].iter_count];
+            ++px_per_iter[values[i][k].iter_count];
         }
     }
 
@@ -103,10 +140,10 @@ int main(int argc, char** argv) {
 
     for (uint k = 0; k < size_y; ++k) {
         for (uint i = 0; i < size_x; ++i) {
-            uint ct = vals[i][k].iter_count;
-            vals[i][k].hue = 0;
+            uint ct = values[i][k].iter_count;
+            values[i][k].hue = 0;
             for (uint l = 0; l < ct; ++l) {
-                vals[i][k].hue += (float)px_per_iter[l] / total;
+                values[i][k].hue += (float)px_per_iter[l] / total;
             }
         }
     }
@@ -115,7 +152,7 @@ int main(int argc, char** argv) {
     if (!output_mode) {
         for (uint k = 0; k < size_y; ++k) {
             for (uint i = 0; i < size_x; ++i) {
-                rgb24 color = palettize(hue_rgb(vals[i][k].hue), palette);
+                rgb24_t color = palettize(hue_rgb(values[i][k].hue), palette);
                 printf("\033[48;2;%i;%i;%im ", color.r, color.g, color.b);
             }
             printf("\033[0m\n");
@@ -125,7 +162,7 @@ int main(int argc, char** argv) {
 
         for (uint k = 0; k < size_y; ++k) {
             for (uint i = 0; i < size_x; ++i) {
-                rgb24 color = palettize(hue_rgb(vals[i][k].hue), palette);
+                rgb24_t color = palettize(hue_rgb(values[i][k].hue), palette);
                 putchar(color.r);
                 putchar(color.g);
                 putchar(color.b);
